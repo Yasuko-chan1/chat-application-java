184c5b497fc04ed82fb809eb3bdf967c6e42d7e8
altynai066 <altynaiserikbai4@gmail.com>
2026-04-18 14:54:36 +0600

    Создан базовый интерфейс JavaFX

---
+++ src/client/ChatWindow.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ChatWindow extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        TextArea chatArea = new TextArea();
+        chatArea.setEditable(false);
+        TextField inputField = new TextField();
+        Button sendButton = new Button("Send");
+
+        VBox root = new VBox(10, chatArea, inputField, sendButton);
+        Scene scene = new Scene(root, 400, 300);
+        
+        primaryStage.setTitle("Java Chat - Client");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
