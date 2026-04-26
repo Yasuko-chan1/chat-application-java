2e2746a49d580459384522b6d22aefd25e29202b
Mahabbat_Toktar <toktarmahabbat507@gmail.com>
2026-04-26 17:24:10 +0500

    настроила базу данных, добавила модели и драйвер mysql

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -1,4 +1,22 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/chat_db";
+    private static final String USER = "root";
+    private static final String PASS = "project";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Драйвер не найден!", e);
+        }
+    }
 }
